4ad547b78bb25a45e53044482143a145bfd040b8
axiomofchoice-hjt <Axiomofchoice@163.com>
2026-01-16 22:04:46 +0800

    refactor: replace direct register access with pointer variables for input and output in scalar and SSEPack kernels

--- src/kernel/kernel_s32_scalar.cpp
+++ src/kernel/kernel_s32_scalar.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <ranges>
 
 #include "kernel_api.h"
@@ -8,10 +7,12 @@ jit_fn<int32_t> gen_kernel<int32_t, ISA::Scalar>(std::span<const size_t> permuta
     size_t target_size = permutation.size();
     return jit::gen<kernel_fn<int32_t>>([=](asmjit::x86::Assembler& as) {
         namespace x86 = asmjit::x86;
+        auto input_ptr = x86::rdi;
+        auto output_ptr = x86::rsi;
 
         for (size_t i : std::views::iota(size_t{0}, target_size)) {
-            as.mov(x86::eax, x86::dword_ptr(x86::rdi, permutation[i] * sizeof(int32_t)));
-            as.mov(x86::dword_ptr(x86::rsi, i * sizeof(int32_t)), x86::eax);
+            as.mov(x86::eax, x86::dword_ptr(input_ptr, permutation[i] * sizeof(int32_t)));
+            as.mov(x86::dword_ptr(output_ptr, i * sizeof(int32_t)), x86::eax);
         }
 
         as.ret();
--- src/kernel/kernel_s32_sse_pack.cpp
+++ src/kernel/kernel_s32_sse_pack.cpp
@@ -8,20 +8,22 @@ jit_fn<int32_t> gen_kernel<int32_t, ISA::SSEPack>(std::span<const size_t> permut
     return jit::gen<kernel_fn<int32_t>>([=](asmjit::x86::Assembler& as) {
         namespace x86 = asmjit::x86;
         constexpr size_t vector_size = 4;
+        auto input_ptr = x86::rdi;
+        auto output_ptr = x86::rsi;
 
         for (size_t i : std::views::iota(size_t{0}, target_size / vector_size * vector_size) | std::views::stride(4)) {
             auto xmm = x86::xmm0;
 
-            as.movd(xmm, x86::dword_ptr(x86::rdi, permutation[i] * sizeof(int32_t)));
-            as.pinsrd(xmm, x86::dword_ptr(x86::rdi, permutation[i + 1] * sizeof(int32_t)), 1);
-            as.pinsrd(xmm, x86::dword_ptr(x86::rdi, permutation[i + 2] * sizeof(int32_t)), 2);
-            as.pinsrd(xmm, x86::dword_ptr(x86::rdi, permutation[i + 3] * sizeof(int32_t)), 3);
-            as.movups(x86::xmmword_ptr(x86::rsi, i * sizeof(int32_t)), xmm);
+            as.movd(xmm, x86::dword_ptr(input_ptr, permutation[i] * sizeof(int32_t)));
+            as.pinsrd(xmm, x86::dword_ptr(input_ptr, permutation[i + 1] * sizeof(int32_t)), 1);
+            as.pinsrd(xmm, x86::dword_ptr(input_ptr, permutation[i + 2] * sizeof(int32_t)), 2);
+            as.pinsrd(xmm, x86::dword_ptr(input_ptr, permutation[i + 3] * sizeof(int32_t)), 3);
+            as.movups(x86::xmmword_ptr(output_ptr, i * sizeof(int32_t)), xmm);
         }
 
         for (size_t i : std::views::iota(target_size / vector_size * vector_size, target_size)) {
-            as.mov(x86::eax, x86::dword_ptr(x86::rdi, permutation[i] * sizeof(int32_t)));
-            as.mov(x86::dword_ptr(x86::rsi, i * sizeof(int32_t)), x86::eax);
+            as.mov(x86::eax, x86::dword_ptr(input_ptr, permutation[i] * sizeof(int32_t)));
+            as.mov(x86::dword_ptr(output_ptr, i * sizeof(int32_t)), x86::eax);
         }
 
         as.ret();
