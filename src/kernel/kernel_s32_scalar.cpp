ace9b152de5611b3640789ae20077036dc603061
axiomofchoice-hjt <Axiomofchoice@163.com>
2026-01-16 21:36:54 +0800

    refactor: remove unused variable

--- src/kernel/kernel_s32_scalar.cpp
+++ src/kernel/kernel_s32_scalar.cpp
@@ -5,7 +5,6 @@
 
 template <>
 jit_fn<int32_t> gen_kernel<int32_t, ISA::Scalar>(std::span<const size_t> permutation) {
-    size_t source_size = *std::ranges::max_element(permutation) + 1;
     size_t target_size = permutation.size();
     return jit::gen<kernel_fn<int32_t>>([=](asmjit::x86::Assembler& as) {
         namespace x86 = asmjit::x86;
